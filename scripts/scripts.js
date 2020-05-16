01a55b495eae7bf263bae7912ae0847327d95820
Melissa Sawyer <melissasawyer013@gmail.com>
2020-05-16 18:08:48 -0400

    updates all classes for better organization, adds hidden div that appears on checkboxes on form to explain more for needs and offerings, adds drowdown feature to community resources navbar link

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,44 @@
-function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+let checkboxes = document.querySelectorAll(".contains-hidden-div");
+for (let i=0; i<checkboxes.length; i++) {
+  console.log(checkboxes[i]);
+  checkboxes[i].addEventListener('click', displayHiddenDiv, false);
+};
+
+function displayHiddenDiv(e) {
+  let checkboxInputToCheck = e.target;
+  console.log(checkboxInputToCheck);
+  let divToShow = checkboxInputToCheck.parentNode.parentNode.children[1];
+  if (checkboxInputToCheck.checked == true) {
+    console.log (`it's checked`);
+    divToShow.style.display = "flex";
+  } else {
+    console.log(`it's not checked`)
+    divToShow.style.display = "none";
   }
-  function onFailure(error) {
-    console.log(error);
-  }
-  function renderButton() {
-    gapi.signin2.render('my-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSuccess,
-      'onfailure': onFailure
-    });
-  }
+}
+
+
+
+
+
+
+// function onSuccess(googleUser) {
+//     console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+//   }
+//   function onFailure(error) {
+//     console.log(error);
+//   }
+//   function renderButton() {
+//     gapi.signin2.render('my-signin2', {
+//       'scope': 'profile email',
+//       'width': 240,
+//       'height': 50,
+//       'longtitle': true,
+//       'theme': 'dark',
+//       'onsuccess': onSuccess,
+//       'onfailure': onFailure
+//     });
+//   }
 
 
 
